df65577edf07314cc637166cd93756a3acdba814
zer0-5 <guilherme2001fernandes@hotmail.com>
2022-04-12 14:14:16 +0100

    Fix typo in help

--- src/yapp.py
+++ src/yapp.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     args_parser = argparse.ArgumentParser(description='yaPP: yet another PreProcessor')
     args_parser.add_argument('input_file', nargs=1, type=str, metavar='input_file', help='input file path')
     args_parser.add_argument('output_file', nargs=1, type=str, metavar='output_file', help='output file path')
-    args_parser.add_argument('-t', '--to', nargs=1, type=str, default='html', choices=['html', 'latex'], help='markup laguage to convert to')
+    args_parser.add_argument('-t', '--to', nargs=1, type=str, default='html', choices=['html', 'latex'], help='markup language to convert to')
 
     args = args_parser.parse_args()
 
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         c = html_converter.HtmlConverter
 
     input = open(args.input_file[0], 'r').read()
-    
+
     p = preprocessor.Preprocessor(c, input)
 
     p.process()
